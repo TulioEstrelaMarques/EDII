18a8a1a3d40232db6df7997a2fd3c9b7504a5602
Túlio Estrela Marques <tulioestrela@hotmail.com>
2022-02-01 15:07:04 -0300

    continuação

--- Listas/Listas/main.c
+++ Listas/Listas/main.c
@@ -2,58 +2,64 @@
 #include <stdlib.h>
 
 //Criando a estrutura...
-typedef struct no
-{
+typedef struct no {
     int valor;
     struct no *proximo;
 } No;
 
-void inserir_inicio(No **lista, int num)
-{
+void inserir_inicio(No **lista, int num) {
     No *nova = malloc(sizeof(No));
-    if(nova)
-    {
+    if(nova) {
         nova->valor = num;
         nova->proximo = *lista;
         *lista = nova;
-    }
-    else
-    {
+    } else {
         printf("Erro ao alocar");
     }
 }
 
-void inserir_fim(No **lista, int num)
-{
+void inserir_fim(No **lista, int num) {
     No *aux,*nova = malloc(sizeof(No));
-    if(nova)
-    {
+    if(nova) {
         nova->valor = num;
         nova->proximo = NULL;
-        if(*lista == NULL)
-        {
+        if(*lista == NULL) {
             *lista = nova;
-        }
-        else
-        {
+        } else {
             aux = *lista;
-            while(aux->proximo)
-            {
+            while(aux->proximo) {
                 aux = aux->proximo;
             }
             aux->proximo = nova;
         }
-    }
-    else
-    {
+    } else {
         printf("Erro ao alocar");
+    }
+}
+
+void inserir_meio(No **lista, int num, int ant) {
+    No *aux, *nova = malloc(sizeof(No));
+    if(nova) {
+        nova->valor = num;
+        if(*lista == NULL) {
+            nova->proximo = NULL;
+            *lista = nova;
+        } else {
+            aux = *lista;
+            while(aux->valor != ant && aux->proximo) {
+                aux = aux->proximo;
+            }
+            nova->proximo = aux->proximo;
+            aux->proximo = nova;
+        }
+    } else {
+        printf("Erro ao alocar.");
     }
 }
 
 
 
 
-int main()
-{
+int main() {
 
 }
