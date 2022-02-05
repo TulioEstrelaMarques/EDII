ddd6e06cb3b050a07d1294e8cd43bdd99b1fde33
Túlio Estrela Marques <tulioestrela@hotmail.com>
2022-02-05 14:42:03 -0300

    upgrade

--- TabelaHash/main.c
+++ TabelaHash/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct no{
+    int key;
+    struct no *prox;
+}No;
+
+typedef struct{
+    No *inicio;
+    int tam;
+}Lista;
+
+void inicializarLista(Lista *l){
+    l->inicio = NULL;
+    l->tam = 0;
+}
+
+void inserirLista(Lista *l, int num){
+    No *nova = malloc(sizeof(No));
+    if(nova){
+        nova->key = num;
+        nova->prox = l->inicio;
+        l->inicio = nova;
+    }else{
+        printf("\nErro ao alocar");
+    }
+}
+
 int main()
 {
     printf("Hello world!\n");
